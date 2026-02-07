f4bc8ab608422594b2e67863bdb928bed0cdd2fd
farhankhan <farhankhan@farhankhans-MacBook-Air.local>
2026-02-07 16:36:49 +0530

    fix firebase

--- go-service/internals/config/firebase_config.go
+++ go-service/internals/config/firebase_config.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"go-servie/utils"
 	"log"
-	"os"
 
 	firebase "firebase.google.com/go/v4"
 	"google.golang.org/api/option"
@@ -13,7 +12,7 @@ import (
 func Firebase_Setup() *firebase.App {
 	firebase_file := utils.FIREBASE_SERVICE
 	opt := option.WithCredentialsFile(firebase_file)
-	config := &firebase.Config{ProjectID: os.Getenv("PROJECT_ID")}
+	config := &firebase.Config{ProjectID: "notev2-34951"}
 	app, err := firebase.NewApp(context.Background(), config, opt)
 	if err != nil {
 		log.Fatalln("Firebase setup failed", err)
